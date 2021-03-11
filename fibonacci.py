5d982a9553ce3612e097ccbdb09db1c6e66144e1
delaneysmith22 <63112746+delaneysmith22@users.noreply.github.com>
2021-03-11 17:26:47 -0500

    Created fibonacci.py
    Created fibonacci.py, after Jose's stupid mistakes. He sucks at Python.

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+
+if __name__=="__main__":
+    x = fib(25)
+    print("stupid fibonacci says:")
+    print(x)
